6b184a73c23ed979e7f238ab2647b8e2a106c86b
davidahiekpor <david.ahiekpor@dreamoval.com>
2023-11-02 15:54:39 +0000

    checkout form

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,12 +2,15 @@ import React, { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
 import CheckoutList from "../components/CheckoutList";
 import { SidebarContext } from "../contexts/SidebarContext";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Col, Row, Select } from "antd";
 import { Link } from "react-router-dom";
+import { Button, Checkbox, Form, Input } from "antd";
 
 const Checkout = () => {
   const { cart, total, itemNumber } = useContext(CartContext);
   const { handleClose } = useContext(SidebarContext);
+  const { Option } = Select;
+
   return (
     <div className="bg-slate-200 h-full pt-[80px]" onClick={handleClose}>
       <div className="container mx-auto  py-16">
@@ -31,25 +34,133 @@ const Checkout = () => {
             </div>
             <div className="mr-5 py-10 w-[450px] px-10 bg-white">
               <div className="flex justify-center">
-                <p className="text-md uppercase  font-medium underline">Summary</p>
+                <p className="text-md uppercase  font-medium underline">
+                  Summary
+                </p>
               </div>
               <div className="flex justify-between mb-2">
                 <h2>Amount</h2>
-                <p className="font-medium"><small>GHS</small> {total}</p>
+                <p className="font-medium">
+                  <small>GHS</small> {total}
+                </p>
               </div>
               <div className="flex justify-between mb-4">
                 <h2>Discount</h2>
-                <p className="font-medium"><small>GHS</small>  0</p>
+                <p className="font-medium">
+                  <small>GHS</small> 0
+                </p>
               </div>
               <div className="flex justify-between mb-4">
                 <h2 className="font-bold text-xl">Total</h2>
-                <p className="font-semibold text-xl text-green-600"><small>GHS</small> {total - 0}</p>
+                <p className="font-semibold text-xl text-green-600">
+                  <small>GHS</small> {total - 0}
+                </p>
               </div>
             </div>
           </div>
 
-          <div style={{ flex: "1" }} className="bg-green-100">
-            Form
+          <div style={{ flex: "1" }} className="">
+            <div className="bg-white max-w-[590px] p-10">
+              <Form
+                name="basic"
+                layout="vertical"
+                onFinish={""}
+                onFinishFailed={""}
+                autoComplete="off"
+              >
+                <Row>
+                  <Col span={11}>
+                    <Form.Item
+                      label="Full Name"
+                      name="fullname"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your full name!",
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Phone Number"
+                      name="phoneNumber"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your phone number!",
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Membership ID" name="membership">
+                      <Input />
+                    </Form.Item>
+                  </Col>
+                  <Col span={1}></Col>
+
+                  <Col span={11}>
+                    <Form.Item
+                      label="Delivery Address"
+                      name="address1"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your full name!",
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item name="address2" label="Street Name">
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Payment Method"
+                      name="paymentMethod"
+                      style={{ marginBottom: "5px" }}
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please select a integration type",
+                        },
+                        {
+                          type: "string",
+                        },
+                      ]}
+                    >
+                      <Select >
+                        <Option value="WALLETSERVICES" style={{ padding: 9 }}>
+                          Cash
+                        </Option>
+                        <Option value="FISERVICES" style={{ padding: 9 }}>
+                          Support Scheme
+                        </Option>
+                      </Select>
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="bg-blue-200"
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
